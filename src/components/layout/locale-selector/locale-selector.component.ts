add679f5b85ec11a7206c8a2feab88d259b533cc
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2024-11-23 21:36:38 +0100

    added logout icon with popup and locale button with popup to select locale

--- src/components/layout/locale-selector/locale-selector.component.ts
+++ src/components/layout/locale-selector/locale-selector.component.ts
@@ -1,20 +1,65 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {PrimeNGConfig} from 'primeng/api';
+import {MenuItem, MessageService, PrimeNGConfig} from 'primeng/api';
 import primeNgEs from "../../../../public/i18n/primeng-es.json";
 import primeNgEn from "../../../../public/i18n/primeng-en.json";
+import {ToastModule} from 'primeng/toast';
+import {MenuModule} from 'primeng/menu';
 
 @Component({
   selector: 'app-locale-selector',
   standalone: true,
-  imports: [],
+  imports: [
+    ToastModule,
+    MenuModule
+  ],
+  providers: [MessageService],
   templateUrl: './locale-selector.component.html',
-  styleUrl: './locale-selector.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LocaleSelectorComponent {
+export class LocaleSelectorComponent implements OnInit {
   private translateService = inject(TranslateService);
   private primeNgConfig = inject(PrimeNGConfig);
+  private messageService = inject(MessageService);
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Español',
+        command: () => {
+          this.spanish();
+        }
+      },
+      {
+        label: 'English',
+        command: () => {
+          this.english();
+        }
+      }
+    ];
+  }
+
+  spanish() {
+    this.useLanguage('es');
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Información',
+      detail: 'El idioma seleccionado es Español',
+      life: 3000
+    });
+  }
+
+  english() {
+    this.useLanguage('en');
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Information',
+      detail: 'The selected language is English',
+      life: 3000
+    });
+  }
 
   useLanguage(language: string): void {
     this.translateService.use(language);
--- src/components/layout/navigation/navigation-bar/navigation-bar.component.ts
+++ src/components/layout/navigation/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,12 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, ViewChild} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {ThemeSelectorComponent} from '../../theme-selector/theme-selector.component';
 import {AuthService} from '../../../../services/auth/auth.service';
 import {TranslatePipe} from '@ngx-translate/core';
 import {LocaleSelectorComponent} from '../../locale-selector/locale-selector.component';
+import {ConfirmationService} from 'primeng/api';
+import {ConfirmPopup, ConfirmPopupModule} from 'primeng/confirmpopup';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -13,11 +16,37 @@ import {LocaleSelectorComponent} from '../../locale-selector/locale-selector.com
     ThemeSelectorComponent,
     LocaleSelectorComponent,
     TranslatePipe,
+    ConfirmPopupModule,
+    Button,
   ],
+  providers: [ConfirmationService],
   templateUrl: './navigation-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavigationBarComponent {
   private authService = inject(AuthService);
+  private confirmationService = inject(ConfirmationService);
   isAuthenticated = this.authService.getIsAuthenticated();
+
+  @ViewChild(ConfirmPopup) confirmPopup!: ConfirmPopup;
+
+  accept() {
+    this.confirmPopup.accept();
+  }
+
+  reject() {
+    this.confirmPopup.reject();
+  }
+
+  confirm(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      accept: () => {
+
+      },
+      reject: () => {
+
+      }
+    });
+  }
 }
