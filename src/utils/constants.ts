fc2912f589a492f31ffe27e8b369fea96b5e49c3
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2024-11-24 13:22:12 +0100

    added more themes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
+// HTTP REQUEST STATUS
 export const SUCCESS = "success";
 export const ERROR = "error";
 export const PENDING = "pending";
-export const THEMES_DARK = ["aura-dark-cyan", "aura-dark-pink", "aura-dark-noir"];
-export const THEMES_LIGHT = ["aura-light-cyan", "aura-light-pink", "aura-light-noir"];
-export const THEMES_DEFAULT = THEMES_DARK[2];
+
+// THEMING
+export const THEMES_DARK = ["aura-dark-cyan", "aura-dark-pink", "aura-dark-noir", "aura-dark-purple", "aura-dark-blue", "aura-dark-amber"];
+export const THEMES_LIGHT = ["aura-light-cyan", "aura-light-pink", "aura-light-noir", "aura-light-purple", "aura-light-blue", "aura-light-amber"];
+export const THEMES_DEFAULT = THEMES_DARK[5];
