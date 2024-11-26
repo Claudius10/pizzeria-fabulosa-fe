be79b537b849c536ff1129c748ddf14b43119b91
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2024-11-26 21:04:08 +0100

    cart icon now lights up when cart is not empty and there's a bump animation each time cartQ changes

--- src/components/layout/navigation/navigation-bar/navigation-bar.component.ts
+++ src/components/layout/navigation/navigation-bar/navigation-bar.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component, inject, Signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, inject, signal, Signal, untracked} from '@angular/core';
 import {AuthService} from '../../../../services/auth/auth.service';
 import {ConfirmationService, MessageService} from 'primeng/api';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+import {NgClass, NgOptimizedImage, NgStyle} from '@angular/common';
 import {TranslatePipe} from '@ngx-translate/core';
 import {LoginDialogComponent} from '../login/login-dialog.component';
 import {LogoutDialogComponent} from '../logout/logout-dialog.component';
@@ -13,6 +13,7 @@ import {SidebarModule} from 'primeng/sidebar';
 import {CartComponent} from '../../../cart/cart.component';
 import {ProgressBarModule} from 'primeng/progressbar';
 import {NavigationService} from '../../../../services/navigation/navigation.service';
+import {CartService} from '../../../../services/cart/cart.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -29,7 +30,9 @@ import {NavigationService} from '../../../../services/navigation/navigation.serv
     ToastModule,
     SidebarModule,
     CartComponent,
-    ProgressBarModule
+    ProgressBarModule,
+    NgStyle,
+    NgClass
   ],
   providers: [ConfirmationService, MessageService],
   templateUrl: './navigation-bar.component.html',
@@ -37,14 +40,40 @@ import {NavigationService} from '../../../../services/navigation/navigation.serv
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavigationBarComponent {
+
   private authService = inject(AuthService);
   private navigationService = inject(NavigationService);
+  private cartService = inject(CartService);
+  cartQuantity = this.cartService.cartQuantity;
   isLoading: Signal<boolean> = this.navigationService.getIsLoading();
   loginDialog: Signal<boolean> = this.authService.getLoginDialog();
   logoutDialog: Signal<boolean> = this.authService.getLogoutDialog();
   isAuthenticated: Signal<boolean> = this.authService.getIsAuthenticated();
   sidebarMobileMenuVisible = false;
   sidebarCartVisible = false;
+  bump = signal(false);
+
+  constructor() {
+    effect((onCleanup) => {
+      if (this.cartQuantity() === 0) {
+        return;
+      }
+
+      untracked(() => {
+        this.bump.set(true);
+      });
+
+      const timer = setTimeout(() => {
+        untracked(() => {
+          this.bump.set(false);
+        });
+      }, 300);
+
+      onCleanup(() => {
+        clearTimeout(timer);
+      });
+    });
+  }
 
   toggleMobileSidePanelMenu() {
     this.sidebarMobileMenuVisible = !this.sidebarMobileMenuVisible;
