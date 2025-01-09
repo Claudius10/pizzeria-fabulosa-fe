8a2c1f0909ab88a3c29aa6b53c968393a1f89922
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2025-01-09 15:38:46 +0100

    styling updating in progress

--- src/components/nav/theme-selector/theme-selector.component.ts
+++ src/components/nav/theme-selector/theme-selector.component.ts
@@ -1,10 +1,13 @@
-import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, HostListener, inject, signal, ViewChild} from '@angular/core';
 import {LocalstorageService} from '../../../services/localstorage/localstorage.service';
 import {ThemeService} from '../../../services/theme/theme.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-theme-selector',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './theme-selector.component.html',
   styleUrls: ['./theme-selector.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -13,6 +16,21 @@ export class ThemeSelectorComponent {
   private localStorageService = inject(LocalstorageService);
   private themeService = inject(ThemeService);
   isDarkMode = signal(this.localStorageService.getDarkMode());
+  visible = false;
+  @ViewChild('themes') themes: ElementRef | undefined;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    if (this.themes) {
+      if (!this.themes.nativeElement.contains(event.target)) {
+        this.visible = false;
+      }
+    }
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
 
   toggleDarkMode = (on: boolean) => {
     this.isDarkMode.set(on);
