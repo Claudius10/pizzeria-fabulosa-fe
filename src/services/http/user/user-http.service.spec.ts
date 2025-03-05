8a0fdb48db1f64b59321d0d72554ef5b0f9503d0
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2025-03-05 16:26:59 +0100

    user-http.service.spec.ts test done

--- src/services/http/user/user-http.service.spec.ts
+++ src/services/http/user/user-http.service.spec.ts
@@ -55,4 +55,105 @@ describe('UserHttpService', () => {
 
     expect(await response).toEqual(buildErrorResponse());
   });
+
+  it('givenCreateUserAddress_thenReturnOk', async () => {
+
+    // Arrange
+
+    const userId = "1";
+    const url = `${PATH + BASE + V1 + USER_BASE}/${userId + USER_ADDRESS}`;
+
+    // Act
+
+    const response$ = service.createUserAddress({
+      userId: userId, data: {
+        id: null,
+        details: "",
+        number: 1,
+        street: ""
+      }
+    });
+
+    const response: Promise<ResponseDTO> = firstValueFrom(response$);
+    const req = httpTesting.expectOne({
+      method: 'POST',
+      url: url
+    });
+
+    const responseValue: ResponseDTO = buildResponse("OK", false, 200, "OK");
+    req.flush(responseValue);
+
+    // Assert
+
+    expect(await response).toEqual(responseValue);
+    httpTesting.verify();
+  });
+
+  it('givenCreateUserAddress_whenUserIdIsNull_thenReturnErrorResponse', async () => {
+
+    // Act
+
+    const response$ = service.createUserAddress({
+      userId: null, data: {
+        id: null,
+        details: "",
+        number: 1,
+        street: ""
+      }
+    });
+
+    const response: Promise<ResponseDTO> = firstValueFrom(response$);
+
+    // Assert
+
+    expect(await response).toEqual(buildErrorResponse());
+  });
+
+  it('givenDeleteUserAddress_thenReturnOk', async () => {
+
+    // Arrange
+
+    const userId = "1";
+    const addressId = "1";
+    const url = `${PATH + BASE + V1 + USER_BASE}/${userId + USER_ADDRESS}/${addressId}`;
+
+    // Act
+
+    const response$ = service.deleteUserAddress({
+      userId: userId, addressId: addressId
+    });
+
+    const response: Promise<ResponseDTO> = firstValueFrom(response$);
+    const req = httpTesting.expectOne({
+      method: 'DELETE',
+      url: url
+    });
+
+    const responseValue: ResponseDTO = buildResponse("OK", false, 200, "OK");
+    req.flush(responseValue);
+
+    // Assert
+
+    expect(await response).toEqual(responseValue);
+    httpTesting.verify();
+  });
+
+  it('givenDeleteUserAddress_whenUserIdIsNull_thenReturnErrorResponse', async () => {
+
+    // Arrange
+
+    const userId = null;
+    const addressId = "1";
+
+    // Act
+
+    const response$ = service.deleteUserAddress({
+      userId: userId, addressId: addressId
+    });
+    const response: Promise<ResponseDTO> = firstValueFrom(response$);
+
+    // Assert
+
+    expect(await response).toEqual(buildErrorResponse());
+  });
 });
