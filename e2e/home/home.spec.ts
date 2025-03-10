53806f029f51ccc6700e62f6d91a1dc7d9832620
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2025-03-10 22:36:58 +0100

    e2e testing in progress

--- e2e/home/home.spec.ts
+++ e2e/home/home.spec.ts
@@ -1,45 +1,59 @@
 import {expect, test} from '@playwright/test';
+import {offers, stores} from '../api-responses';
 
-test('givenHomePage_thenDisplayOffers', async ({page}) => {
-  await page.route('*/**/api/v1/resource/offer', async route => {
-    const json = {
-      "timeStamp": "2025-03-09T19:37:30.463882738",
-      "status": {"code": 200, "description": "OK", "error": false},
-      "payload": [{
-        "id": 1,
-        "image": "/assets/offers/offer.jpeg",
-        "name": {"es": "3x2 en Pizzas", "en": "3x2 for Pizzas"},
-        "description": {
-          "es": "Válida todos los días en Pizzas Medianas y Familiares",
-          "en": "Valid every day for Medium and Familiar sized Pizzas"
-        },
-        "caveat": {
-          "es": "Cualquiera especialidad o hasta 4 ingredientes",
-          "en": "Any specialty or up to 4 ingredients"
-        }
-      }, {
-        "id": 2,
-        "image": "/assets/offers/offer.jpeg",
-        "name": {"es": "2ª Pizza al 50%", "en": "Second Pizza at half the price"},
-        "description": {
-          "es": "Válida todos los días en Pizzas Medianas y Familiares",
-          "en": "Valid every day for Medium and Familiar sized Pizzas"
-        },
-        "caveat": {
-          "es": "Cualquiera especialidad o hasta 4 ingredientes",
-          "en": "Any specialty or up to 4 ingredients"
-        }
-      }],
-      "error": null
-    };
-    await route.fulfill({json});
+test.describe('API OK', () => {
+  test.beforeEach(async ({page}) => {
+    await page.route('*/**/api/v1/resource/offer', async route => {
+      await route.fulfill({json: offers});
+    });
+
+    await page.route('*/**/api/v1/resource/store', async route => {
+      await route.fulfill({json: stores});
+    });
+
+    await page.goto('/');
   });
-  // Go to the page
-  await page.goto('/');
 
-  const offerItem = page.getByTestId('OfferItem');
+  test('givenHomePage_thenWelcomeText', async ({page}) => {
+    await expect(page.getByText('Welcome to')).toBeVisible();
+    await expect(page.getByText('Pizzeria Fabulosa')).toBeVisible();
+  });
 
-  // Assert that the Strawberry fruit is visible
-  await expect(page.getByText('3x2 for Pizzas')).toBeVisible();
-  await expect(page.getByText('Second Pizza at half the price')).toBeVisible();
+  test('givenHomePage_thenDisplayOffers', async ({page}) => {
+
+
+    await expect(page.getByText('Discover our offers')).toBeVisible();
+    await expect(page.getByText('3x2 for Pizzas')).toBeVisible();
+    await expect(page.getByText('Second Pizza at half the price')).toBeVisible();
+    await expect(page.getByText('Valid every day for Medium and Familiar sized Pizzas').first()).toBeVisible();
+    await expect(page.getByText('Conditions').first()).toBeVisible();
+    await expect(page.getByAltText('Offer Picture').nth(0)).toBeVisible();
+    await expect(page.getByAltText('Offer Picture').nth(1)).toBeVisible();
+  });
+
+  test('givenHomePage_thenDisplayStores', async ({page}) => {
+
+    await expect(page.getByText('Come by our store')).toBeVisible();
+    await expect(page.getByText('Alustre', {exact: true})).toBeVisible();
+    await expect(page.getByText('Avenida Alustre 15')).toBeVisible();
+    await expect(page.getByText('666555666')).toBeVisible();
+    await expect(page.getByText('Viciosa', {exact: true})).toBeVisible();
+    await expect(page.getByText('Calle Viciosa 221')).toBeVisible();
+    await expect(page.getByText('555666555')).toBeVisible();
+    await expect(page.getByText('Schedule').first()).toBeVisible();
+    await expect(page.getByTitle('Phone').nth(0)).toBeVisible();
+    await expect(page.getByTitle('Phone').nth(1)).toBeVisible();
+    await expect(page.getByTitle('Location').nth(0)).toBeVisible();
+    await expect(page.getByTitle('Location').nth(1)).toBeVisible();
+    await expect(page.getByAltText('Store Location on Map').nth(0)).toBeVisible();
+    await expect(page.getByAltText('Store Location on Map').nth(1)).toBeVisible();
+  });
+});
+
+test.describe('API KO', () => {
+  test('givenHomePage_whenApiIsDown_thenDisplayServerError', async ({page}) => {
+    await page.goto('/');
+    await expect(page.getByText('Our servers are not available at the moment').first()).toBeVisible({timeout: 10_000});
+    await expect(page.getByText('Please try again later. We apologize for any inconvenience.').first()).toBeVisible({timeout: 10_000});
+  });
 });
