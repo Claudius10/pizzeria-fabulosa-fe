11c239c90744d8c072a48500d0ecaf892b1e8dcd
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2025-04-01 22:45:58 +0200

    repair after merge

--- src/components/user/dashboard/profile/user-address/user-address-item/user-address-item.component.ts
+++ src/components/user/dashboard/profile/user-address/user-address-item/user-address-item.component.ts
@@ -1,14 +1,17 @@
 import {ChangeDetectionStrategy, Component, inject, input, OnDestroy} from '@angular/core';
-import {AddressDTO} from '../../../../../utils/interfaces/dto/order';
+
 import {Button} from 'primeng/button';
-import {ResponseDTO} from '../../../../../utils/interfaces/http/api';
-import {LoadingAnimationService} from '../../../../../services/animation/loading-animation.service';
-import {MutationRequest, MutationResult} from '../../../../../utils/interfaces/mutation';
+
 import {TranslatePipe} from '@ngx-translate/core';
 import {injectMutation, QueryClient} from '@tanstack/angular-query-experimental';
 import {lastValueFrom} from 'rxjs';
 import {USER_ADDRESS_LIST} from '../../../../../../utils/query-keys';
 import {UserHttpService} from '../../../../../../services/user/http/user/user-http.service';
+import {MutationRequest, MutationResult} from '../../../../../../utils/interfaces/mutation';
+import {LoadingAnimationService} from '../../../../../../services/common/animation/loading-animation.service';
+import {AddressDTO} from '../../../../../../utils/interfaces/dto/order';
+import {ResponseDTO} from '../../../../../../utils/interfaces/http/api';
+import {ErrorService} from '../../../../../../services/common/error/error.service';
 
 @Component({
   selector: 'app-user-address-item',
@@ -21,8 +24,10 @@ import {UserHttpService} from '../../../../../../services/user/http/user/user-ht
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserAddressItemComponent implements OnDestroy {
+  address = input.required<AddressDTO>();
   private loadingAnimationService = inject(LoadingAnimationService);
   private userHttpService = inject(UserHttpService);
+  private errorService = inject(ErrorService);
   private queryClient = inject(QueryClient);
   private deleteUserAddress: MutationResult = injectMutation(() => ({
     mutationFn: (request: MutationRequest) => lastValueFrom(this.userHttpService.deleteUserAddress(request.payload)),
@@ -30,7 +35,6 @@ export class UserAddressItemComponent implements OnDestroy {
       this.queryClient.refetchQueries({queryKey: USER_ADDRESS_LIST});
     }
   }));
-  address = input.required<AddressDTO>();
 
   ngOnDestroy(): void {
     this.loadingAnimationService.stopLoading();
@@ -41,8 +45,12 @@ export class UserAddressItemComponent implements OnDestroy {
 
     this.deleteUserAddress.mutate({payload: id.toString()}, {
       onSuccess: (response: ResponseDTO) => {
+        if (response && response.status.error && response.error) {
+          this.errorService.handleError(response.error);
+        }
       },
       onError: () => {
+        this.errorService.handleServerNoResponse();
       },
       onSettled: () => {
         this.loadingAnimationService.stopLoading();
